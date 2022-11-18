dd5485f7c06e924b441253cce0877535dcfffba8
briankaney <kaneybrian@gmail.com>
2022-11-18 13:56:36 -0600

    Another util converted to huge file, tab option form

--- text_column_utils/query_empty_columns.php
+++ text_column_utils/query_empty_columns.php
@@ -22,8 +22,8 @@
     print "Options:\n";
     print "  Use '-h=2' to specify a number of header lines.  The default is zero.  The header section\n";
     print "  is skipped in the analysis.  The default delimiter is the pipe character, but can be changed\n";
-    print "  via '-d=comma'.  Space delimited files can't support 'empty' fields.  The option '-ignore-last'\n";
-    print "  can be used to skip any reports of missing fields in the last field.\n\n";
+    print "  via '-d=comma' or '-d=tab'.  Space delimited files can't support 'empty' fields.  The option\n";
+    print "  '-ignore-last' can be used to skip any reports of missing fields in the last field.\n\n";
     exit(0);
   }
 
@@ -53,37 +53,47 @@
   }
 
 //--------------------------------------------------------------------------------------
-//   Read in contents of input file
+//   Open input file, skip header lines.
 //--------------------------------------------------------------------------------------
 
-  $lines = file("$infile",FILE_IGNORE_NEW_LINES);
-  $fields = SplitLinesToFields($lines,$header,$delimiter);
-  $num_lines = count($fields);
+  $inf = fopen($infile,'r');
+
+  for($i=0;$i<$header;++$i)
+  {
+    $line = fgets($inf);
+    print "$line";
+  }
 
 //--------------------------------------------------------------------------------------
+//   Loop through input lines, explode into fields, loop through fields, report empties.
 //--------------------------------------------------------------------------------------
 
   print "\n";
-  for($i=0;$i<$num_lines;++$i) {
-    $num_fields = count($fields[$i]);
+  $i=0;
+  while( ($line = fgets($inf)) !== false)
+  {      
+    $fields = SplitOneLineToFields($line,$delimiter);
+    $num_fields = count($fields);
 
     $empties_found = false;
-    for($j=0;$j<$num_fields;++$j) {
-      if(strlen($fields[$i][$j])==0) {
+    for($j=0;$j<$num_fields;++$j)
+    {
+      if(strlen($fields[$j])==0)
+      {
         if($j==$num_fields-1 && $mode=="ignore") { continue; }
 
-        if($empties_found==false) {
+	if($empties_found==false)
+	{
           $str = sprintf("Line %d empties: %d ",$i+1,$j);
           print "Line $i empties: ";
-          }
+        }
         $empties_found = true;
         print "$j ";
       }
     }
     if($empties_found==true) { print "\n"; }
+    ++$i;
   }
   print "\n";
-
-//--------------------------------------------------------------------------------------
 
 ?>
